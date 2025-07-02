5dd2543eba33e952dec18456e53fc7920efd2434
Zodiacal0 <javierherrera5513@gmail.com>
2025-07-01 18:32:36 -0600

    feat(accounts): accounts y favAccounts

--- src/pages/moneyAccounts/moneyAcountsPage.jsx
+++ src/pages/moneyAccounts/moneyAcountsPage.jsx
@@ -2,8 +2,10 @@ import Navbar from "../../components/navbar.jsx";
 import Sidebar from "../../components/sideBar.jsx";
 import { FiCreditCard, FiHeart } from "react-icons/fi";
 import "../../assets/styles/accountCards.css";
+import { useNavigate } from "react-router-dom";
 
 const MoneyAccountsPage = () => {
+  const navigate = useNavigate()
   return (
     <>
       <Navbar />
@@ -15,7 +17,7 @@ const MoneyAccountsPage = () => {
             width: "100%",
             padding: "2rem 0 0 2rem",
             background: "#F5F5F7",
-            minHeight: "100vh",
+            minHeight: "100vh"
           }}
         >
           <h2
@@ -25,13 +27,13 @@ const MoneyAccountsPage = () => {
             Cuentas de Dinero
           </h2>
           <div className="cards-grid">
-            <div className="account-card info">
+            <div className="account-card info" onClick={() => { navigate("/my-accounts") }}>
               <div className="card-icon">
                 <FiCreditCard />
               </div>
               <h3 className="card-title">Mis cuentas</h3>
             </div>
-            <div className="account-card favorites">
+            <div className="account-card favorites" onClick={() => { navigate("/my-favorite-accounts") }}>
               <div className="card-icon">
                 <FiHeart />
               </div>
