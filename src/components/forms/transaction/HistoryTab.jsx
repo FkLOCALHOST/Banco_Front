b26244660d9035204eb8f48eeb98ceef2b564347
Zodiacal0 <javierherrera5513@gmail.com>
2025-07-02 21:50:22 -0600

    feat(transactions): revert

---
+++ src/components/forms/transaction/HistoryTab.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const HistoryTab = () => {
+  return (
+    <div className="history-container">
+      <div className="history-header">
+        <h3>Últimas transacciones</h3>
+        <button className="view-all-btn">Ver todo</button>
+      </div>
+    </div>
+  );
+};
+
+export default HistoryTab;
