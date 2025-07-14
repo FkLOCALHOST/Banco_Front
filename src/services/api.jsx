10e2a8da577a7c92ebf3d628f94c918fedda5d08
Zodiacal0 <javierherrera5513@gmail.com>
2025-07-13 22:12:01 -0600

    feat(api): login

--- src/services/api.jsx
+++ src/services/api.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiWallet = axios.create({
-  baseURL: "http://localhost:3005/walletManager/v1",
+  baseURL: "https://banco-api-eta.vercel.app/walletManager/v1",
   timeout: 9000,
   withCredentials: true, // Permite enviar cookies con cada solicitud
 });
